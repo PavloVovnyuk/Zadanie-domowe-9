33f5301153a3e305b8b3e185d3a0f7b49593c6bf
Pavlo <pawel.wowniuk@gmail.com>
2019-02-07 08:03:24 +0100

    Zadanie domowe 9 przeroboine

--- src/Test.java
+++ src/Test.java
@@ -2,19 +2,15 @@ public class Test {
     public static void main(String[] args) {
 
         Car[] cars = new Car[3];
-        cars[0] = new Car ("BMW", 40, 15, false);
+        cars[0] = new Car ("BMW", 40, 10, false);
         cars[1] = new Car ("Audi", 35, 13, false);
-        cars[2] = new Truck ("Volvo", 90, 35, false, 5000);
-        double newBurn = 0;
+        cars[2] = new Truck ("Volvo", 40, 10,  false, 5000);
+        double newRange = 0;
         for (int i = 0; i < cars.length; i++) {
             if (cars[i] != null) {
                 cars[i].shovInfo ();
-                if (cars[i].turnOnCond ()) {
-                    cars[i].calculate ();
-                    newBurn = cars[i].calculate ();
-                    System.out.println ("Spałanie przy włącznej klimatyzacji wynosi " + newBurn);
-
-                }
+                newRange=cars[i].calculateRange ();
+                System.out.println ("Zasieng wynosi " + newRange + " km" );
             }
         }
     }
